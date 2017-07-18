e9714da27ac5073ef51b55028b2d0624628b9ddb
David Ludwig <davidludwigii@gmail.com>
2017-07-18 16:06:42 -0500

    Adjusted Minecraft server status command

--- src/commands/minecraft_command.py
+++ src/commands/minecraft_command.py
@@ -5,12 +5,22 @@ from mcstatus import MinecraftServer
 class MinecraftCommand(Command):
 
 	def __init__(self):
-		self.__server = MinecraftServer.lookup("survival.intercraftmc.com")
+		self.__survivalServer = MinecraftServer.lookup("survival.intercraftmc.com")
+		self.__creativeServer = MinecraftServer.lookup("creative.intercraftmc.com")
 
 
 	def status(self, message):
-		status = self.__server.status()
-		result = 'Latency: ' + str(status.latency) + 'ms\n'
+
+		result = ""
+		if message.content == 'creative':
+			result = "Status of creative server:\n\n"
+			server = self.__creativeServer
+		else:
+			result = "Status of survival server:\n\n"
+			server = self.__survivalServer
+
+		status = server.status()
+		result += 'Latency: ' + str(status.latency) + 'ms\n'
 		result += 'Players online: ' + str(status.players.online)
 		return result
 
