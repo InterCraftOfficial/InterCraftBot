266f7c2d0682a1638a2e3645c6ac5c3f32ab98a4
David Ludwig <davidludwigii@gmail.com>
2017-07-18 15:01:03 -0500

    Oops, indented wrong

--- src/commands/touch_command.py
+++ src/commands/touch_command.py
@@ -2,10 +2,10 @@ from commands.command import *
 
 class TouchCommand(Command):
 
-  def privates(self, message):
-    return "Don't touch my privates you perverted dickcheese"
+	def privates(self, message):
+		return "Don't touch my privates you perverted dickcheese"
 
-  def run(self, message):
-    return "Don't touch me you pervert"
+	def run(self, message):
+		return "Don't touch me you pervert"
 
 commandClass = TouchCommand
