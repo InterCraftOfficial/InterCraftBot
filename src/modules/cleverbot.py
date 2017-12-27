3660f7b6de694ee99bd0ee8da58c69d5f32bb626
David Ludwig <intercraft@davidludwig.net>
2017-12-27 09:43:27 -0600

    A few adjustments

--- src/modules/cleverbot.py
+++ src/modules/cleverbot.py
@@ -6,10 +6,14 @@ class Cleverbot:
 	def __init__(self, apiKey):
 		self.__apiKey = apiKey
 		self.__sessions = {}
-		
+
 
 	def send(self, author, message):
 		if author not in self.__sessions:
 			print("Adding new author")
 			self.__sessions[author] = CleverWrap(self.__apiKey)
-		return self.__sessions[author].say(message)
+		try:
+			return self.__sessions[author].say(message)
+		except Exception as e:
+			print(e)
+			return None
